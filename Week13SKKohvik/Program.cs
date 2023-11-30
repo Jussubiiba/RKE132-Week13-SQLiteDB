01402f54ab9a4ca38d936ed8a6a2edbf7330ce45
Mitovski <marvin.mit@tktk.ee>
2023-11-30 20:03:40 +0200

    add week13 code

---
+++ Week13SKKohvik/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SQLite;
+
+FindCustomer (CreateConnection());
+
+static SQLiteConnection CreateConnection()
+{
+    SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; Version = 3; New = True; Compress = True;");
+
+    try
+    {
+        connection.Open();
+        Console.WriteLine("DB found.");
+    }
+    catch
+    {
+        Console.WriteLine("DB not found.");
+    }
+
+    return connection;
+}
+
+
+static void FindCustomer(SQLiteConnection myConnection)
+{
+    SQLiteDataReader reader;
+    SQLiteCommand command;
+    String searchName;
+    Console.WriteLine("Enter a first name to display customer data:");
+    searchName = Console.ReadLine();
+    command = myConnection.CreateCommand();
+    command.CommandText = $"SELECT customer.rowid, customer.firstname, customer.lastname, status.statustype FROM customerStatus JOIN customer ON customer.rowid = customerStatus.customerid JOIN status on status.rowid = customerStatus.statusid WHERE firstname LIKE '{searchName}'";
+    reader = command.ExecuteReader();
+    while (reader.Read())
+    {
+        string readerRowid = reader["rowid"].ToString();
+        string readerStringName = reader.GetString(1);
+        string readerStringLastName = reader.GetString(2);
+        string readerStringStatus = reader.GetString(3);
+        Console.WriteLine($"Search result: ID: {readerRowid}. {readerStringName} {readerStringLastName}. Status: {readerStringStatus}");
+    }
+    myConnection.Close();
+}
